ffc556b7e5160999d6c26ff9780d0c3fb03ca4a4
Rafael Guedes <Guedes@MBP-de-Rafael.home>
2018-10-22 20:21:43 +0100

    Not found heading color fix

--- app/components/NotFound.js
+++ app/components/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NotFound() {
     return(
         <div>
-            <h1>Ups. Page not found</h1>
+            <h1 className='heading-not-found'>Ups. Page not found</h1>
         </div> 
     )
 }
