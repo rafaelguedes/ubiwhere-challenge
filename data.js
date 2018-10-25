7804b296d36eb69aa2fb5dc23a1310d093bd0840
Rafael Guedes <Guedes@MBP-de-Rafael.home>
2018-10-25 18:07:35 +0100

    Express api serving react files

---
+++ data.js
@@ -0,0 +1,206 @@
+var data = {
+    users: [{
+        "id": 1,
+        "username": "admin",
+        "email": "admin",
+    }],
+    musics: [{
+        "genre": "Hip-Hop",
+        "albums": [{
+            "id": 1,
+            "genre": "Hip-Hop",
+            "artist": "Kanye West",
+            "title": "808s & Heartbreak",
+            "date": "November 2008",
+            "producer": "The Carter Administrations, Kanye West et al",
+            "bio": "Kanye West is a Grammy Award-winning rapper and record producer and fashion designer. Outspoken and prone to controversy, he became additional celebrity fodder with his marriage to Kim Kardashian.",
+            "albumCover": "808.png",
+            "artistPic": "KanyeWest.png"
+        }, {
+            "id": 2,
+            "genre": "Hip-Hop",
+            "artist": "Kendrick Lamar",
+            "title": "Good Kid M.A.AD City",
+            "date": "October 2012",
+            "producer": "Anthony “Top Dawg” Tiffith, Dr Dre et al",
+            "bio": "Kendrick Lamar is an award-winning rapper and songwriter best known for his innovative take on life in Southern California as well as for his affiliation with rap star and super-producer Dr. Dre.",
+            "albumCover": "GoodKidM.A.A.DCity.png",
+            "artistPic": "KendrickLamar.png"
+        }, {
+            "id": 3,
+            "genre": "Hip-Hop",
+            "artist": "NAS",
+            "title": "IllMatic",
+            "date": "April 1994",
+            "producer": "Faith N, Mc Serch et al",
+            "bio": "East Coast rapper Nas made it big with albums such as Illmatic, It Was Written and Stillmatic. East Coast rapper Nas made it big with albums such as Illmatic, It Was Written and Stillmatic.",
+            "albumCover": "IllMatic.png",
+            "artistPic": "NAS.png"
+        }, {
+            "id": 4,
+            "genre": "Hip-Hop",
+            "artist": "Biggie Smalls",
+            "title": "Ready To Die",
+            "date": "Setember 1994",
+            "producer": "Mr Cee, Bluez Brothers et al",
+            "bio": "Biggie Smalls, also known as 'The Notorious B.I.G.', was a revered hip-hop artist and face of East Coast gangsta rap. He was shot and killed on March 9, 1997 in Los Angels.",
+            "albumCover": "ReadyToDie.png",
+            "artistPic": "Biggie.png"
+        }, {
+            "id": 5,
+            "genre": "Hip-Hop",
+            "artist": "Drake",
+            "title": "Take Care",
+            "date": "November 2011",
+            "producer": "Aubrey “Drake” Graham, Noah 40 Sebib et al",
+            "bio": "Artic Monkeys are an English rock band formed in 2002 in High Green, a suburb of Sheffield. The band consists of Alex Turner, Matt Helders, Jamie Cook and Nick O'Malley. Former band member Andy Nicholson left the band in 2006 shortly after their debut album was released.",
+            "albumCover": "TakeCare.png",
+            "artistPic": "Drake.png"
+        }, {
+            "id": 6,
+            "genre": "Hip-Hop",
+            "artist": "Dr. Dre",
+            "title": "The Chronic",
+            "date": "June 1992",
+            "producer": "Dr Dre & Suge Knight",
+            "bio": "Rapper-producer Dr. Dre first made it big with hip-hop group N.W.A. in the 1980s. He has also enjoyed success as a solo act and worked with Snoop Dogg, Eminem and 50 Cent.",
+            "albumCover": "TheChronic.png",
+            "artistPic": "DrDre.png"
+        }]
+    }, {
+        "genre": "Indie",
+        "albums": [{
+            "id": 7,
+            "genre": "Indie",
+            "artist": "Artic Monkeys",
+            "title": "AM",
+            "date": "Setember 2013",
+            "producer": "James Ford & Ross Orton",
+            "bio": "Arctic Monkeys are an English rock band formed in 2002 in High Green, a suburb of Sheffield. The band consists of Alex Turner, Matt Helders, Jamie Cook and Nick O'Malley. Former band member Andy Nicholson left the band in 2006 shortly after their debut album was released.",
+            "albumCover": "AM.png",
+            "artistPic": "ArticMonkeys.png"
+        }, {
+            "id": 8,
+            "genre": "Indie",
+            "artist": "MGMT",
+            "title": "Congratulations",
+            "date": "April 2010",
+            "producer": "Peter Kember",
+            "bio": "MGMT is an American rock band formed in 2002 in Middletown, Connecticut. It currently consists of Andrew VanWyngarden and Ben Goldwasser. In the live lineup it consists of VanWyngarden, Goldwasser, Will Berman, Simon O'Connor, and James Richardson.",
+            "albumCover": "Congratulations.png",
+            "artistPic": "MGMT.png"
+        }, {
+            "id": 9,
+            "genre": "Indie",
+            "artist": "Arcade Fire",
+            "title": "The Suburbs",
+            "date": "August 2010",
+            "producer": "Markus Dravs",
+            "bio": "Arcade Fire is a Canadian indie rock band, consisting of husband and wife Win Butler and Régine Chassagne, along with Win's younger brother William Butler, Richard Reed Parry, Tim Kingsbury and Jeremy Gara.",
+            "albumCover": "TheSuburbs.png",
+            "artistPic": "ArcadeFire.png"
+        }, {
+            "id": 10,
+            "genre": "Indie",
+            "artist": "Pixies",
+            "title": "Doolittle",
+            "date": "April 1989",
+            "producer": "Gil Norton",
+            "bio": "The Pixies are an American alternative rock band formed in 1986 in Boston, Massachusetts. Until 2013, the band comprised Black Francis, Joey Santiago, Kim Deal and David Lovering. The band disbanded acrimoniously in 1993, but reunited in 2004.",
+            "albumCover": "Doolittle.png",
+            "artistPic": "Pixies.png"
+        }, {
+            "id": 11,
+            "genre": "Indie",
+            "artist": "Bon Iver",
+            "title": "Holocene",
+            "date": "June 2011",
+            "producer": "Justin Vernon",
+            "bio": "Bon Iver is an American indie folk band founded in 2006 by singer-songwriter Justin Vernon. The name 'Bon Iver' derives from the French phrase bon hiver, taken from a greeting on Northern Exposure. Vernon released Bon Iver's debut album, For Emma, Forever Ago, independently in July 2007.",
+            "albumCover": "Holocene.png",
+            "artistPic": "BonIver.png"
+        }, {
+            "id": 12,
+            "genre": "Indie",
+            "artist": "The Strokes",
+            "title": "Is This It",
+            "date": "July 2001",
+            "producer": "Gordon Raphael",
+            "bio": "The Strokes are an American rock band from New York City. Formed in 1998, the band is composed of singer Julian Casablancas, lead guitarist Nick Valensi, rhythm guitarist Albert Hammond Jr., bassist Nikolai Fraiture, and drummer Fabrizio Moretti.",
+            "albumCover": "IsThisIt.png",
+            "artistPic": "TheStrokes.png"
+        }]
+    }, {
+        "genre": "Rock",
+        "albums": [{
+            "id": 13,
+            "genre": "Rock",
+            "artist": "The Velvet Underground",
+            "title": "Velvet Underground",
+            "date": "March 1969",
+            "producer": "The Velvet Underground",
+            "bio": "The Velvet Underground was an American rock band formed in 1964 in New York City by singer/guitarist Lou Reed, multi-instrumentalist John Cale, guitarist Sterling Morrison, and drummer Angus MacLise.",
+            "albumCover": "VelvetUnderground.png",
+            "artistPic": "TheVelvetUnderground.png"
+        }, {
+            "id": 14,
+            "genre": "Rock",
+            "artist": "The Smashing Pumpkins",
+            "title": "Mellon Collie",
+            "date": "October 1995",
+            "producer": "Alan Moulder, Billy Corgan et al",
+            "bio": "The Smashing Pumpkins are an American alternative rock band from Chicago, Illinois. Formed in 1988 by frontman Billy Corgan, D'arcy Wretzky, James Iha, and Jimmy Chamberlin, the band has undergone many line-up changes. The current lineup features Corgan, Chamberlin, Iha and guitarist Jeff Schroeder.",
+            "albumCover": "MellonCollie.png",
+            "artistPic": "TheSmashingPumpkins.png"
+        }, {
+            "id": 15,
+            "genre": "Rock",
+            "artist": "Blink 182",
+            "title": "Enema of the State",
+            "date": "June 1999",
+            "producer": "Jerry Finn",
+            "bio": "Blink-182 is an American rock band formed in Poway, California, in 1992. Since 2015, the lineup of the band has consisted of bassist and vocalist Mark Hoppus, drummer Travis Barker, and guitarist and vocalist Matt Skiba.",
+            "albumCover": "EnemaoftheState.png",
+            "artistPic": "Blink182.png"
+        }, {
+            "id": 16,
+            "genre": "Rock",
+            "artist": "LedZepplin",
+            "title": "House of the Holy Led",
+            "date": "March 1973",
+            "producer": "Jimmy Page",
+            "bio": "Led Zeppelin were an English rock band formed in London in 1968. The group consisted of guitarist Jimmy Page, singer Robert Plant, bassist and keyboardist John Paul Jones, and drummer John Bonham. The band's heavy, guitar-driven sound has led them to be cited as one of the progenitors of heavy metal.",
+            "albumCover": "HouseoftheHolyLed.png",
+            "artistPic": "LedZepplin.png"
+        }, {
+            "id": 17,
+            "genre": "Rock",
+            "artist": "Nirvana",
+            "title": "Nevermind",
+            "date": "Setember 2011",
+            "producer": "Butch Vig",
+            "bio": "Nirvana was an American rock band formed by lead singer and guitarist Kurt Cobain and bassist Krist Novoselic in Aberdeen, Washington, in 1987. Nirvana went through a succession of drummers, the longest-lasting and best-known being Dave Grohl, who joined in 1990.",
+            "albumCover": "Nevermind.png",
+            "artistPic": "Nirvana.png"
+        }, {
+            "id": 18,
+            "genre": "Rock",
+            "artist": "Pink Floyd",
+            "title": "Dark Side of the Moon",
+            "date": "March 1973",
+            "producer": "Pink Floyd",
+            "bio": "Pink Floyd were an English rock band formed in London in 1965. They achieved international acclaim with their progressive and psychedelic music. Distinguished by their philosophical lyrics, sonic experimentation, extended compositions, and elaborate live shows, they are one of the most commercially successful and influential groups in popular music history.",
+            "albumCover": "TheDarkSideoftheMoon.png",
+            "artistPic": "PinkFloyd.png"
+        }]
+    }],
+    favorites: [{
+        userid: 1,
+        musicid: 1
+    }, {
+        userid: 1,
+        musicid: 2
+    }]
+};
+
+module.exports = data;
